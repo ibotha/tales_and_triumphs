32606ec06b4a75350a845b2d082c82bc30a33974
Isard Botha <isard.botha@gmail.com>
2022-07-30 12:15:55 +0200

    Move to rte tools

--- Frontend/main.ts
+++ Frontend/main.ts
@@ -1,10 +1,8 @@
 import "./style.scss";
 import { Elm } from "./src/Main.elm";
-import EditorJS, { Tool } from "@editorjs/editorjs";
-import Header from "@editorjs/header";
-import Underline from "@editorjs/underline";
-import Checklist from "@editorjs/checklist";
-import List from "@editorjs/list";
+import '@webcomponents/webcomponentsjs/webcomponents-bundle.js'
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js'
+import 'elm-rte-toolkit';
 
 if (process.env.NODE_ENV === "development") {
   const ElmDebugTransform = await import("elm-debug-transformer");
@@ -13,26 +11,6 @@ if (process.env.NODE_ENV === "development") {
     simple_mode: true,
   });
 }
-
-customElements.define('tnt-editor',
-    class extends HTMLElement {
-        // things required by Custom Elements
-        constructor() { super(); }
-        connectedCallback() {
-          this.onkeydown = (e) => {
-            e.preventDefault();
-          }
-        }
-        attributeChangedCallback() { this.setTextContent(); }
-        static get observedAttributes() { return ['class']; }
-
-        // Our function to set the textContent based on attributes.
-        setTextContent()
-        {
-          
-        }
-    }
-);
 
 const root = document.querySelector("#app div");
 const app = Elm.Main.init({ node: root });
--- Frontend/src/Main.elm
+++ Frontend/src/Main.elm
@@ -1,44 +1,60 @@
 module Main exposing (main)
 
+import Array
 import Browser
-import Html exposing (Html, div, h1, h2, h3, h4, h5, h6, li, ol, text, ul)
-import Html.Attributes exposing (class, contenteditable)
-import Html.Events exposing (preventDefaultOn)
-import Json.Decode as Decode
-import Keyboard.Event exposing (KeyboardEvent, decodeKeyboardEvent)
+import Html exposing (Html, div, text)
+import RichText.Commands exposing (defaultCommandMap)
+import RichText.Config.Decorations exposing (emptyDecorations)
+import RichText.Definitions exposing (doc, markdown, paragraph)
+import RichText.Editor exposing (Config, Editor, Message, config, init, update, view)
+import RichText.Model.Element as Element
+import RichText.Model.Node
+    exposing
+        ( Block
+        , Children(..)
+        , Inline(..)
+        , block
+        , blockChildren
+        , inlineChildren
+        , plainText
+        )
+import RichText.Model.State exposing (state)
 
 
 main : Program () Model Msg
 main =
-    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+    Browser.element { init = myInit, update = myUpdate, view = myView, subscriptions = subscriptions }
 
 
 
 --Init
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
-    ( [ Header "Jensin" 1, Text "Jensin is a boi that can do things.", List Unordered [ "The", "Cow", "Jumped", "Over" ] ], Cmd.none )
+docNode : Block
+docNode =
+    block
+        (Element.element doc [])
+        (blockChildren <|
+            Array.fromList
+                [ block
+                    (Element.element paragraph [])
+                    (inlineChildren <| Array.fromList [ plainText "Hello world" ])
+                ]
+        )
+
+
+myInit : () -> ( Model, Cmd Msg )
+myInit _ =
+    ( { editor = init <| state docNode Nothing }, Cmd.none )
 
 
 
 -- Model
 
 
-type ListType
-    = Unordered
-    | Ordered
-
-
-type ContentBlock
-    = Header String Int
-    | Text String
-    | List ListType (List String)
-
-
 type alias Model =
-    List ContentBlock
+    { editor : Editor
+    }
 
 
 
@@ -46,9 +62,7 @@ type alias Model =
 
 
 type Msg
-    = HandleKeyboardEvent KeyboardEvent
-    | InsertNewBlock
-    | NoOp
+    = InternalMessage Message
 
 
 
@@ -64,99 +78,23 @@ subscriptions _ =
 -- Update
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+myConfig : Config Msg
+myConfig =
+    config
+        { decorations = emptyDecorations
+        , commandMap = defaultCommandMap
+        , spec = markdown
+        , toMsg = InternalMessage
+        }
+
+
+myUpdate : Msg -> Model -> ( Model, Cmd Msg )
+myUpdate msg model =
     case msg of
-        HandleKeyboardEvent e ->
-            Debug.log (Debug.toString e)
-                ( model, Cmd.none )
-
-        InsertNewBlock ->
-            ( List.append model [ Text "New" ], Cmd.none )
-
-        NoOp ->
-            ( model, Cmd.none )
+        InternalMessage internalEditorMsg ->
+            ( { model | editor = update myConfig internalEditorMsg model.editor }, Cmd.none )
 
 
-listElementView : String -> Html Msg
-listElementView str =
-    li [] [ text str ]
-
-
-blockView : ContentBlock -> Html Msg
-blockView block =
-    case block of
-        Header content level ->
-            (case level of
-                1 ->
-                    h1
-
-                2 ->
-                    h2
-
-                3 ->
-                    h3
-
-                4 ->
-                    h4
-
-                5 ->
-                    h5
-
-                6 ->
-                    h6
-
-                _ ->
-                    h1
-            )
-                [ contenteditable True, class "editor-block" ]
-                [ text content ]
-
-        Text content ->
-            div [ contenteditable True, class "editor-block" ] [ text content ]
-
-        List ltype content ->
-            (case ltype of
-                Ordered ->
-                    ol
-
-                Unordered ->
-                    ul
-            )
-                [ contenteditable True, class "editor-block" ]
-                (List.map listElementView content)
-
-
-mapEditorEvent : KeyboardEvent -> ( Msg, Bool )
-mapEditorEvent rawEvent =
-    case rawEvent.key of
-        Just key ->
-            case key of
-                "Enter" ->
-                    if rawEvent.shiftKey then
-                        ( NoOp, False )
-
-                    else
-                        ( InsertNewBlock, True )
-
-                _ ->
-                    ( NoOp, False )
-
-        _ ->
-            ( NoOp, False )
-
-
-editor : Model -> Html Msg
-editor model =
-    div
-        [ class "editor"
-        , preventDefaultOn "keydown" <|
-            Decode.map mapEditorEvent decodeKeyboardEvent
-        ]
-        [ div [] (List.map blockView model)
-        ]
-
-
-view : Model -> Html Msg
-view model =
-    editor model
+myView : Model -> Html Msg
+myView model =
+    div [] [ view myConfig model.editor ]
